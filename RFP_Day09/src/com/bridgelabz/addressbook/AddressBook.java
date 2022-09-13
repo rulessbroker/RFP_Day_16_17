456b8669bb6362326e0ed58bed169d187a6d5ebf
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-13 16:59:34 +0530

    Ability to create a Contacts

--- RFP_Day09/src/com/bridgelabz/addressbook/AddressBook.java
+++ RFP_Day09/src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
+	
 	public static void main(String[] args) {
 		System.out.println("*****Welecome To Address Book Program*****");
+		
+		System.out.println("\n --Enter your contact details here---> \n");
+		
+		contacts.contact();
 	}
+	
+	
 }

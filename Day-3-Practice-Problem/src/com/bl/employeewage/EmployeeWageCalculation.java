c0b7909b101b2d1970e351a51d467e58c4695c8f
Rahul Bhosale <109344583+rulessbroker@users.noreply.github.com>
2022-09-28 19:30:52 +0530

    Delete

--- Day-3-Practice-Problem/src/com/bl/employeewage/EmployeeWageCalculation.java
+++
@@ -1,7 +0,0 @@
-package com.bl.employeewage;
-
-public class EmployeeWageCalculation {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-	}
-}
